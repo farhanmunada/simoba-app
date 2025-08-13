3f5176c7bd604ef112c514d2e8c5ac3cb5fda022
farhanmunada <farchanang@gmail.com>
2025-08-13 08:20:45 +0700

    menambah logo pada halaman

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,6 +12,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
     <!-- Google Fonts - Inter -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('Simoba.ico') }}" type="image/x-icon">
 
     <style>
         :root {
